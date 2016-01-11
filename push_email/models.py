504af1d0b5a7d92f1796c5baba6ca25f4d1ee736
hannon <hannonq@yahoo.com.br>
2016-01-11 16:18:50 -0200

    added email models

--- push_email/models.py
+++ push_email/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Recipient(models.Model):
+    email_to = models.EmailField(null=False)
+    email = models.ForeignKey('MyEmail', on_delete=models.CASCADE) # one email can one or more recipients
+
+class Copy(models.Model):
+    copy_to = models.EmailField(null=True)
+    email = models.ForeignKey('MyEmail', on_delete=models.CASCADE) # one email can have zero or more CC
+
+class MyEmail(models.Model):
+    email_subject = models.CharField(null=True, max_length=255)
+    email_from = models.EmailField()
+    email_body = models.TextField(null=True)
